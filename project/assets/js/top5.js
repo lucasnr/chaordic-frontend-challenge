d9330a74b23a644792c4290a041e682772b5e501
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-02-11 17:00:16 -0300

    Consuming the json with fetch
    I'm not sure about browser support for arrow functions so i avoided using them

---
+++ project/assets/js/top5.js
@@ -0,0 +1,37 @@
+const list = document.querySelector("#top-5-list");
+
+fetch(
+	"https://raw.githubusercontent.com/chaordic/frontend-intern-challenge/master/Assets/urls.json"
+)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(shortedLinks) {
+		const sorted = shortedLinks.sort(function(a, b) {
+			return a.hits > b.hits ? -1 : 1;
+		});
+		const top5 = sorted.slice(0, 5).map(function(link) {
+			return {
+				...link,
+				hits: new Intl.NumberFormat("pt-BR").format(link.hits)
+			};
+		});
+		top5.forEach(function(link) {
+			const item = document.createElement("li");
+			item.classList.add("top-5__item");
+
+			const anchor = document.createElement("a");
+			anchor.setAttribute("target", "_blank");
+			anchor.setAttribute("href", link.url);
+			anchor.innerText = link.shortUrl;
+			anchor.classList.add("top-5__item__link");
+
+			const span = document.createElement("span");
+			span.innerText = link.hits;
+			span.classList.add("top-5__item__amount");
+
+			item.appendChild(anchor);
+			item.appendChild(span);
+			list.appendChild(item);
+		});
+	});
